0ae0a9896fc8606abe1845f074afc0dfea4e0895
Ifetayo02 <abdulqoyumjamiu@gmail.com>
2026-04-16 13:45:23 +0100

    commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {postSignup,getSignup,getSignin, getDashboard,postSignin} = require('../controllers/user.controller');
+const {postSignup,getSignup,getSignin, getDashboard,postSignin,getAllUsers} = require('../controllers/user.controller');
 
 router.get('/signUp', getSignup);
 router.post('/register', postSignup);
